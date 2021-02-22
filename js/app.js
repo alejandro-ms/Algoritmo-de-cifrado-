f4e88412d3e3ad4c92e987db29c466b5d46f9342
Alejandro Miranda <ams-180700@hotmail.com>
2021-02-22 11:11:49 -0700

    Se modificó el id del output y se agregaron comentarios

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 function encript (element) {
-   let word = document.getElementsByTagName("input")[0].value;
-   let word_encrypted = btoa(word);
-   document.getElementById("decrypt").innerHTML = word_encrypted;
+   let word = document.getElementsByTagName("input")[0].value;  //Se obtiene el elemento input como solamente hay un input es el de la posición 0
+   let word_encrypted = btoa(word); // btoa para codificar en base-64
+   document.getElementById("encript").innerHTML = word_encrypted;
  }
 
  function decrypt(element) {
-    let word = document.getElementsByTagName("output")[0].value;
-    let word_decrypt = atob(word);
-    document.getElementById("result").innerHTML = word_decrypt;
+    let word = document.getElementsByTagName("output")[0].value; // Se obtiene el elemento output como solamente hay un output es el de la posición 0
+    let word_decrypt = atob(word); //atob para decodificar 
+    document.getElementById("result").innerHTML = word_decrypt; //se obtiene el elemento con id result que es un output y se manda la palabra desencriptada
  }
-
+ 
 
  
